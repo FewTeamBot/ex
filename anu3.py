ca2f9405bd704b74e58db0f6ee7582da0c262de4
ariezta <42648618+ariezta@users.noreply.github.com>
2018-08-26 22:29:49 +0700

    Update anu3.py

--- anu3.py
+++ anu3.py
@@ -43,6 +43,7 @@ channelToken = channel.getChannelResult()
 clientMid = client.profile.mid
 clientPoll = OEPoll(client)
 clientProfile = client.getProfile()
+clientStart = time.time()
 contact = client.getProfile()
 backup = client.getProfile()
 backup.displayName = contact.displayName
@@ -233,6 +234,82 @@ def clientBot(op):
 							}
 							data = json.dumps(jsonData)
 							sendPost = _session.post(url, data=data, headers=headers)
+						elif cmd == "me":
+							_session = requests.session()
+							image = "https://lh3.googleusercontent.com/proxy/-qcXIaVI5RPLI_rZgSi8T-QyHCDuVXRoFQUksJ2tzKKOGt8vGLQ6EW7yZBO9SIpQ0b5GlZgahj8S4lENJRr2PDK7jN-vPImkR628uGfvOlr3HpSjBCWrGfCGiOsj9pT7PjH8OuZ6bZ7_9RB7tTeUcmld8U5z=w256-h256-nc"
+							url = "https://game.linefriends.com/jbp-lcs-ranking/lcs/sendMessage"
+							headers = {
+								"Host": "game.linefriends.com",
+								"Content-Type": "application/json",
+								"User-Agent": "Mozilla/5.0",
+								"Referer": "https://game.linefriends.com/cdn/jbp-lcs/"
+							}
+							jsonData = {
+								"cc": channelToken.token,
+								#contact = client.getContact(msg._from)
+								"to": to,
+								"messages": [
+                                                              {
+                                                                   "type": "flex",
+                                                                   "altText": "AR SAYANG KAMU",
+                                                                   "contents": {
+                                                                       "type": "bubble",
+                                                                       "hero": {
+                                                                               "type": "image",
+                                                                               "url": "https://obs.line-scdn.net/{}".format(contact.picturePath),
+                                                                               "size": "full",
+                                                                               "aspectRatio": "1.51:1",
+                                                                               "aspectMode": "cover",
+                                                                       },
+                                                                       "body": {
+                                                                               "type": "box",
+                                                                               "layout": "vertical",
+                                                                               "contents": [
+                                                                                  {
+                                                                                    "type": "text",
+                                                                                    "text": "Detail Profile",
+                                                                                    "size": "xl",
+                                                                                    "weight": "bold",
+                                                                                    "wrap": True
+                                                                                  },
+                                                                                  {
+                                                                                    "type": "text",
+                                                                                    "text": " MID:\n {}\n\n Nama:\n  {}\n\nStatus:\n  {}".format(contact.mid,contact.displayName,contact.statusMessage),
+                                                                                    "wrap": True
+                                                                                  }
+                                                                               ]
+                                                                       },
+                                                                       "footer": {
+                                                                               "type": "box",
+                                                                               "layout": "vertical",
+                                                                               "spacing": "md",
+                                                                               "contents": [
+                                                                                  {
+                                                                                    "type": "button",
+                                                                                    "style": "primary",
+                                                                                    "action": {
+                                                                                        "type": "uri",
+                                                                                        "label": "Profile",
+                                                                                        "uri": "line://nv/profile"
+                                                                                     }
+                                                                                  },
+                                                                                  {
+                                                                                    "type": "button",
+                                                                                    "style": "primary",
+                                                                                    "action": {
+                                                                                        "type": "uri",
+                                                                                        "label": "Creator",
+                                                                                        "uri": "https://line.me/ti/p/~mase-pesek"
+                                                                                     }
+                                                                                  }
+                                                                               ]
+                                                                       }
+                                                                   }
+                                                              }
+                                                           ]
+                                                       }	
+							data = json.dumps(jsonData)
+							sendPost= _session.post(url, data=data, headers=headers)	
 						elif cmd == "me.":
 							_session = requests.session()
 							image = "https://lh3.googleusercontent.com/proxy/-qcXIaVI5RPLI_rZgSi8T-QyHCDuVXRoFQUksJ2tzKKOGt8vGLQ6EW7yZBO9SIpQ0b5GlZgahj8S4lENJRr2PDK7jN-vPImkR628uGfvOlr3HpSjBCWrGfCGiOsj9pT7PjH8OuZ6bZ7_9RB7tTeUcmld8U5z=w256-h256-nc"
